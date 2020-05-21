1ba84a834b27302c47c3bc3326dd4afd8035fc96
Austin Barrett <austinbrrtt@gmail.com>
2020-05-20 23:33:11 -0400

    #172831560 - Convert status flags to status enum

--- UpNext/Domain/ItemStatus.swift
+++ UpNext/Domain/ItemStatus.swift
@@ -12,4 +12,30 @@ enum ItemStatus: String {
     case unstarted = "UNSTARTED"
     case started = "STARTED"
     case completed = "COMPLETED"
+    
+    // returns the next status, looping around from the end
+    func next() -> ItemStatus {
+        switch self {
+        case .unstarted:
+            return .started
+        case .started:
+            return .completed
+        case .completed:
+            return .unstarted
+        }
+    }
+}
+
+extension ItemStatus: Comparable {
+    // .unstarted < .started < .completed
+    static func < (lhs: ItemStatus, rhs: ItemStatus) -> Bool {
+        switch lhs {
+        case .unstarted:
+            return rhs != .unstarted
+        case .started:
+            return rhs == .completed
+        case .completed:
+            return false
+        }
+    }
 }
